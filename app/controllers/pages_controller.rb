ced0fc84f98fb139b3cd871b56099479cf6b9cc7
Victor Park <victor.shpark@gmail.com>
2020-03-11 16:41:34 -0300

    button colors changed, placeholders in forms placed.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,6 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:home]
 
   def home
-    redirect_to profile_path if current_user
+    redirect_to new_survey_path if current_user
   end
 end
