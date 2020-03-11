36d70692db9f2ea19a0d90ef217c6c562309ccfe
Victor Park <victor.shpark@gmail.com>
2020-03-11 18:45:41 -0300

    Beeeeeeeeeeeeeautiful alerts

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,6 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:home]
 
   def home
-    redirect_to new_survey_path if current_user
+    redirect_to dashboard_path if current_user
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -12,4 +12,5 @@ Rails.application.routes.draw do
   end
 
   get 'profile', to: 'profiles#show'
+  get 'dashboard', to: 'dashboards#show'
 end
