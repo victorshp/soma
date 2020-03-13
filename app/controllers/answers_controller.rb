0cafc29dced9360be3fd2d8b4f2f33802c027678
Fabio Seiji dos Santos <fabio1987_@hotmail.com>
2020-03-13 17:48:39 -0300

    Made a lot of changes in a lot of views and controllers

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -2,12 +2,20 @@ class AnswersController < ApplicationController
 before_action :get_question, only: [:create]
 
   def create
-    @answer = Answer.new(answer_params)
-    @answer.question = @question
-    @survey = @question.survey
+    @survey = Question.find(params[:answer].keys.first).survey
     survey_answer = SurveyAnswer.where(user_id: current_user, survey_id: @survey.id ).first
-    @answer.survey_answer_id = survey_answer.id
-    @answer.save
+
+    params[:answer].each do |question_id, answer_content|
+      answer = Answer.new
+      answer.content = answer_content
+      answer.question_id = question_id
+      answer.survey_answer_id = survey_answer.id
+      if !answer.save
+        render('survey_answers/new', alert: 'Please answer all the questions!') and return
+      end
+    end
+
+    redirect_to profile_path, notice: 'Thanks for answering! You received R$ 0,30'
   end
 
   private
